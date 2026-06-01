ef79296bea0b08c5a9908e399f275bdb9be4375e
FranklynaDoh-Nani <dohnanifranklyna@gmail.com>
2026-06-01 21:03:42 +0000

    Add files via upload

---
+++ initials.py
@@ -0,0 +1,32 @@
+# File: initials.py
+# Description: Print out my initials.
+#Assignment Number: 2
+#
+#Name: Doh-Nani Franklyna
+#STUDENT ID:2425404882
+#Email:2425404882@live.gctu.edu.gh
+#Grader: Mr. Augustus Buckman
+#
+#On my honor, Doh-Nani Franklyna, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+def main():
+    # Print my initials in large letters
+
+    print()
+    print("...DNF")
+    print()
+
+    print("...DDDDDDDDDD..........NN........NN........FFFFFFFFFFFF.....")
+    print("...DD.......DD.........NNN.......NN........FF...............")
+    print("...DD........DD........NNNN......NN........FF...............")
+    print("...DD........DD........NN.NN.....NN........FFFFFFFFFFFF.....")
+    print("...DD........DD........NN..NN....NN........FF...............")
+    print("...DD........DD........NN...NN...NN........FF...............")
+    print("...DD........DD........NN....NN..NN........FF...............")
+    print("...DD.......DD.........NN.....NN.NN........FF...............")
+    print("...DD......DD.....**...NN.......NNN...**...FF.............**")
+    print("...DDDDDDDDD......**...NN........NN...**...FF.............**")
+    print()
+
+main()
